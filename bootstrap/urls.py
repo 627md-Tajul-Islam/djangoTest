c2d6ae475c96793618854f98045f21e430eafebb
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-10-07 23:29:46 -0700

    done

--- bootstrap/urls.py
+++ bootstrap/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'bootstrap'
+
 urlpatterns = [
-    path('bootstrap/', views.index, name='bla bla' ),
+    path('bootstrap/', views.index, name='index' ),
 ]
